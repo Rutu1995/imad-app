1e42513b650e62f6ddb150a295d24c9f3e39c9e2
Rutu1995 <30718359+Rutu1995@users.noreply.github.com>
2017-08-06 11:16:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article one',function (req,res){
+app.get('/Article-one',function (req,res){
     res.sendFile('Article one requested and will be served here');
     });
     
-app.get('/Article two',function (req,res){
+app.get('/Article-two',function (req,res){
     res.sendFile('Article two requested and will be served here');
     });
     
-app.get('/Article three',function (req,res){
+app.get('/Article-three',function (req,res){
     res.sendFile('Article three requested and will be served here');
     });
     
